b0746ea577da334cdac16c3a41aada5afdd718f4
Raul <raulbv.personal@gmail.com>
2021-08-12 15:47:48 +1200

    Update stage_redshift.py

--- src/plugins/operators/stage_redshift.py
+++ src/plugins/operators/stage_redshift.py
@@ -1,24 +1,111 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+import boto3
 
+def create_iam_role(aws_key_id, aws_secret,
+                    rolename = "redhisft-airflow"):
+    """
+    Doc-string function later
+    """
+    
+    iam = boto3.client('iam',
+                        region_name='us-west-2',
+                        aws_access_key_id=aws_key_id,
+                        aws_secret_access_key=aws_secret)
+    
+    print('Creating new IAM role')
+    try:
+        print('Creating new IAM role')
+        airflow_role = iam.create_role(Path='/',
+                                RoleName=rolename,
+                                Description="Allows Redshift clusters to call AWS services on your behalf.",
+                                AssumeRolePolicyDocument=json.dumps({'Statement': [{'Action': 'sts:AssumeRole',
+                                                                                    'Effect': 'Allow',
+                                                                                    'Principal': {'Service': 'redshift.amazonaws.com'}}],
+                                                                                    'Version': '2012-10-17'}))
+        print("IAM policy was created succesfully")
+        
+    except Exception as e:
+        print(e)
+        
+    print('Attaching Policy:')
+    iam.attach_role_policy(RoleName= rolename,
+                        PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")['ResponseMetadata']['HTTPStatusCode']
+    
+    role_arn = iam.get_role(RoleName=rolename)['Role']['Arn']
+    
+    return role_arn
+
+
+def create_redshift_cluster(aws_key_id, aws_secret, role_arn,
+                            cluster_type=""
+                            node_type="", 
+                            n_nodes=1,
+                            db_name="airflow",
+                            cluster_id="",
+                            db_user="airflow",
+                            db_pwd="airflow"):
+    """
+    Doc-string function later
+    """
+    
+    redshift = boto3.client('redshift',
+                            region_name='us-west-2',
+                            aws_access_key_id=aws_key_id,
+                            aws_secret_access_key=aws_secret)
+    
+    print(f"Creating Redshift cluster:")
+    try:
+        response = redshift.create_cluster(ClusterType=cluster_type,
+                                        NodeType = node_type,
+                                        NumberOfNodes=int(n_nodes),
+                                        # Identifiers and credentials
+                                        DBName=db_name,
+                                        ClusterIdentifier=cluster_id,
+                                        MasterUsername=db_user,
+                                        MasterUserPassword=db_pwd,
+                                        # Roles (for s3 access)
+                                        IamRoles = [role_arn])
+        
+    except Exception as e:
+        print(e)
+    print(f"Cluster was created succesfully")
+    # Obtaining redshift_conn_id
+    ### TODO: Look at dl.cfg and extract CLUSTER values
+    
+    
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="udacity-dend",
+                 s3_key="",
+                 delimiter=",",
+                 ignore_headers=1,
                  *args, **kwargs):
-
-        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
 
     def execute(self, context):
         self.log.info('StageToRedshiftOperator not implemented yet')
+        self.table = table
+        self.s3_key = s3_key
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.aws_credentials_id = aws_credentials_id
+        
+        # Extract aws key and secret
+        #### TODO
+        aws_key_id, aws_secret = '####'
+        
+        # Create aws resources on the fly
+        role_arn = create_iam_role(aws_key_id, aws_secret)
+        redshift_conn_id = create_redshift_cluster(aws_key_id, aws_secret, role_arn)
+
+
 
 
 
