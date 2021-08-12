294197111f6eb752d12be6c7afb4f2d0dfb2522e
Raul <raulbv.personal@gmail.com>
2021-08-13 11:09:11 +1200

    Update load_dimension.py

--- src/plugins/operators/load_dimension.py
+++ src/plugins/operators/load_dimension.py
@@ -8,15 +8,32 @@ class LoadDimensionOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table=""
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+         # Create PostreSQL connection and load fact data
+        self.log.info('Create PostreSQL connection')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        # Determine what dimension has to be loaded
+        if self.table == "users":
+            sql_dim_load = S3ToRedshiftOperator.users_table_insert 
+        elif self.table == "song":
+            sql_dim_load = S3ToRedshiftOperator.song_table_insert 
+        elif self.table == "artist":
+            sql_dim_load = S3ToRedshiftOperator.artist_table_insert 
+        elif self.table == "time":
+            sql_dim_load = S3ToRedshiftOperator.artist_table_insert
+        else:
+            self.log.info('Error: Dimension Table to be loaded was not defined')
+            sql_dim_load = None
+            
+        # Execute load sql command for dim table
+        self.log.info('Loading data into select dim table')
+        redshift.run(sql_dim_load)
