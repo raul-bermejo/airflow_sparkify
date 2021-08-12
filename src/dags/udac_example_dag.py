5aa1397383d1ff08c1b4d81b1ad5dfa00dd0efa0
Raul <raulbv.personal@gmail.com>
2021-08-13 11:12:43 +1200

    Update udac_example_dag.py

--- src/dags/udac_example_dag.py
+++ src/dags/udac_example_dag.py
@@ -50,21 +50,81 @@ load_songplays_table = LoadFactOperator(
 )
 
 load_user_dimension_table = LoadDimensionOperator(
+    from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    'owner': 'raulbv',
+    'start_date': datetime(2021, 8, 11),
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    
+}
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *'
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    aws_credentials='####',
+    table='staging_events',
+    s3_key='log_data'
+    task_id='Stage_events',
+    dag=dag
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    aws_credentials='####',
+    table='staging_songs',
+    s3_key='song_data'
+    task_id='Stage_songs',
+    dag=dag
+)
+
+load_songplays_table = LoadFactOperator(
+    redshift_conn_id="#####",
+    task_id='Load_songplays_fact_table',
+    dag=dag
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    redshift_conn_id="#####",
+    table="users",
     task_id='Load_user_dim_table',
     dag=dag
 )
 
 load_song_dimension_table = LoadDimensionOperator(
+    redshift_conn_id="#####",
+    table="song",
     task_id='Load_song_dim_table',
     dag=dag
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
+    redshift_conn_id="#####",
+    table="artist",
     task_id='Load_artist_dim_table',
     dag=dag
 )
 
 load_time_dimension_table = LoadDimensionOperator(
+    redshift_conn_id="#####",
+    table="time",
     task_id='Load_time_dim_table',
     dag=dag
 )
