84fa5aa80eb214f36efe1ce5361950460bbdb065
raul-bermejo <raulbv.personal@gmail.com>
2021-08-15 21:14:02 +1200

    Tidied up udac_example_dag.py

--- src/dags/udac_example_dag.py
+++ src/dags/udac_example_dag.py
@@ -2,9 +2,8 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
-from helpers import SqlQueries
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator, LoadDimensionOperator, DataQualityOperator)
+# from helpers import SqlQueries
 
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
@@ -19,7 +18,7 @@ default_args = {
     
 }
 
-dag = DAG('udac_example_dag',
+dag = DAG('sparkify_pipeline',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
           schedule_interval='0 * * * *'
@@ -30,7 +29,7 @@ start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 stage_events_to_redshift = StageToRedshiftOperator(
     aws_credentials='####',
     table='staging_events',
-    s3_key='log_data'
+    s3_key='log_data',
     task_id='Stage_events',
     dag=dag
 )
@@ -38,100 +37,48 @@ stage_events_to_redshift = StageToRedshiftOperator(
 stage_songs_to_redshift = StageToRedshiftOperator(
     aws_credentials='####',
     table='staging_songs',
-    s3_key='song_data'
+    s3_key='song_data',
     task_id='Stage_songs',
     dag=dag
 )
 
 load_songplays_table = LoadFactOperator(
-    redshift_conn_id="#####",
+    redshift_conn_id="redshift",
     task_id='Load_songplays_fact_table',
     dag=dag
 )
 
 load_user_dimension_table = LoadDimensionOperator(
-    from datetime import datetime, timedelta
-import os
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
-from helpers import SqlQueries
-
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
-
-default_args = {
-    'owner': 'raulbv',
-    'start_date': datetime(2021, 8, 11),
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5),
-    
-}
-
-dag = DAG('udac_example_dag',
-          default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-        )
-
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
-
-stage_events_to_redshift = StageToRedshiftOperator(
-    aws_credentials='####',
-    table='staging_events',
-    s3_key='log_data'
-    task_id='Stage_events',
-    dag=dag
-)
-
-stage_songs_to_redshift = StageToRedshiftOperator(
-    aws_credentials='####',
-    table='staging_songs',
-    s3_key='song_data'
-    task_id='Stage_songs',
-    dag=dag
-)
-
-load_songplays_table = LoadFactOperator(
-    redshift_conn_id="#####",
-    task_id='Load_songplays_fact_table',
-    dag=dag
-)
-
-load_user_dimension_table = LoadDimensionOperator(
-    redshift_conn_id="#####",
+    redshift_conn_id="redshift",
     table="users",
     task_id='Load_user_dim_table',
     dag=dag
 )
 
 load_song_dimension_table = LoadDimensionOperator(
-    redshift_conn_id="#####",
+    redshift_conn_id="redshift",
     table="song",
     task_id='Load_song_dim_table',
     dag=dag
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
-    redshift_conn_id="#####",
+    redshift_conn_id="redshift",
     table="artist",
     task_id='Load_artist_dim_table',
     dag=dag
 )
 
 load_time_dimension_table = LoadDimensionOperator(
-    redshift_conn_id="#####",
+    redshift_conn_id="redshift",
     table="time",
     task_id='Load_time_dim_table',
     dag=dag
 )
 
 run_quality_checks = DataQualityOperator(
-    redshift_conn_id="#####",
-    table=["users", "song", "artist", "time"]
+    redshift_conn_id="redshift",
+    table=["users", "song", "artist", "time"],
     task_id='Run_data_quality_checks',
     dag=dag
 )
