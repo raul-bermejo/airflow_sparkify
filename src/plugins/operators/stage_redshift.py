ab96d0fc10109871e0ec4ce97098cb65a8c58422
raul-bermejo <raulbv.personal@gmail.com>
2021-08-17 09:06:34 +1200

    Updated repo

--- src/plugins/operators/stage_redshift.py
+++ src/plugins/operators/stage_redshift.py
@@ -8,13 +8,13 @@ from airflow.utils.decorators import apply_defaults
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
     copy_sql = """
-        COPY {}
-        FROM '{}'
-        ACCESS_KEY_ID '{}'
-        SECRET_ACCESS_KEY_ID '{}'
-        {}
-        ;
-    """
+                COPY {}
+                FROM '{}'
+                ACCESS_KEY_ID '{}'
+                SECRET_ACCESS_KEY '{}'
+                FORMAT AS JSON '{}'
+                ;
+               """
     
     @apply_defaults
     def __init__(self,
@@ -24,6 +24,7 @@ class StageToRedshiftOperator(BaseOperator):
                  s3_bucket = "",
                  s3_key="",
                  table = "",
+                 json_type = "auto",
                  extra_params = "",
                  *args, **kwargs):
         
@@ -35,6 +36,7 @@ class StageToRedshiftOperator(BaseOperator):
         self.s3_key = s3_key
         self.table = table
         self.extra_params = extra_params
+        self.json_type = json_type
         
     def execute(self, context):
         
@@ -49,12 +51,14 @@ class StageToRedshiftOperator(BaseOperator):
         #Staging Data from S3 to Redshift
         rendered_key = self.s3_key.format(**context)
         s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
+
+        # Format sql insert statement and run redshift_hook
         formatted_sql = StageToRedshiftOperator.copy_sql.format(
             self.table,
             s3_path,
             credentials.access_key,
             credentials.secret_key,
             self.ignore_headers,
-            self.extra_params
+            self.json_type
         )
         redshift.run(formatted_sql)
